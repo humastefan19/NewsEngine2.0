b63fd02f0148536bfa00f5643ad4532e962c6016
Your Name <you@example.com>
2019-12-01 20:00:18 +0200

    requirements set on ProposedNews

--- NewsEngine2.0/Models/ProposedNews.cs
+++ NewsEngine2.0/Models/ProposedNews.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace NewsEngine2._0.Models
 {
@@ -11,9 +12,16 @@ namespace NewsEngine2._0.Models
         [Key]
         public int ProposedNewsId { get; set; }
         [Required]
-        public int UserId{ get; set; }
+        public string UserId {get; set; }
+        [Required(ErrorMessage ="Nu ati introdus titlul")]
         public string Title { get; set; }
+        [Required(ErrorMessage = "Nu ati introdus continutul")]
         public string Content { get; set; }
+        [Required]
         public int CategoryId { get; set; }
+
+        public ApplicationUser User { get; set; }
+        public Category Category { get; set; }
+        public IEnumerable<SelectListItem> Categories { get; set; }
     }
 }
