96e0a66a404a596fd584c6b917c7c0c2407decdb
Your Name <you@example.com>
2019-12-01 20:16:17 +0200

    Comment requirements added

--- NewsEngine2.0/Models/Comment.cs
+++ NewsEngine2.0/Models/Comment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +8,13 @@ namespace NewsEngine2._0.Models
 {
     public class Comment
     {
+        [Key]
         public int CommentId { get; set; }
+        [Required]
         public int UserId{ get; set; }
-        public string Subject { get; set; }
+        [Required (ErrorMessage ="Nu ati introdus comentariu")]
         public string Content { get; set; }
+
+        public ApplicationUser User{ get; set; }
     }
 }
